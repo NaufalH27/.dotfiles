f566e315b4da093cae212e2a494596dda8cef4c0
NaufaH27 <naufalhmuzakki@gmail.com>
2026-01-28 18:11:11 +0700

    prevent minimize for pinned workspace

--- .config/hypr/scripts/hide_window.sh
+++ .config/hypr/scripts/hide_window.sh
@@ -5,5 +5,10 @@ MINIMIZED_WS_OFFSET=10
 active_ws=$(hyprctl activeworkspace -j | jq '.id')
 hidden_ws=$((active_ws + MINIMIZED_WS_OFFSET))
 
-hyprctl dispatch movetoworkspacesilent "$hidden_ws"
+active=$(hyprctl activewindow -j)
+is_pinned=$(jq -r '.pinned' <<<"$active")
+
+if [[ "$is_pinned" == "false" ]]; then
+    hyprctl dispatch movetoworkspacesilent "$hidden_ws"
+fi
 
